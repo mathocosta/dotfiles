2bb5f49b93268a91fe27ee8c701ab6f9669666d7
Matheus Oliveira Costa <matheusoliveiracos@gmail.com>
2019-07-10 11:45:55 -0300

    Initial commit

---
+++ homebrew/brew.sh
@@ -0,0 +1,19 @@
+
+brew install caskroom/cask/brew-cask
+
+brew cask install spotify
+brew cask install firefox
+brew cask install dropbox
+
+brew cask install sublime-text
+brew cask install sourcetree
+brew cask install docker
+brew cask install node
+
+# iOS development
+brew install carthage
+brew install cocoapods
+brew cask install fastlane
+
+brew tap caskroom/fonts
+brew cask install font-fira-code
