9e964309d622cb7d6aff872114d583217d74dc2c
Matheus Oliveira Costa <matheusoliveiracos@gmail.com>
2020-03-12 17:55:29 -0300

    Adds brew casks

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -1,16 +1,27 @@
 
-brew install caskroom/cask/brew-cask
+brew install vim
+brew install grep
+brew install openssh
+brew install travis
+brew install node
+brew install yarn
+brew install git-flow
 
 brew cask install spotify
-brew cask install firefox
 brew cask install dropbox
 
 brew cask install sublime-text
+brew cask install visual-studio-code
 brew cask install sourcetree
+brew cask install postman
 brew cask install docker
-brew cask install node
+brew cask install telegram-desktop
+
+brew tap heroku/brew
+brew install heroku
 
 # iOS development
+brew install swiftlint
 brew install carthage
 brew install cocoapods
 brew cask install fastlane
