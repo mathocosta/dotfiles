fe4820aaf49e96d48ebdddff1302190327c5ae96
Matheus Costa <matheusoliveiracos@gmail.com>
2024-04-08 10:19:08 -0300

    wip: add placeholder files

--- macos/link.sh
+++ macos/link.sh
@@ -1,5 +1,5 @@
 # Symlink config files
 ln -sv "${PWD}/git/.gitconfig" ~
-ln -sv "${PWD}/git/.gitignore_global" ~
+ln -sv "${PWD}/.gitignore_global" ~
 ln -sv "${PWD}/zsh/.zshrc" ~
 ln -sv "${PWD}/zsh/zsh_aliases.sh" ~
